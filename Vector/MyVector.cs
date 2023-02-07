468b8f0523f98c2476285dfd187c817359d3ea47
Daria <kukuruzadasha86@gmail.com>
2023-02-07 17:50:09 +0200

    Add project files.

---
+++ Vector/MyVector.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1.1.Члены данных:
+Закрытые:
+Координата х - 
+Координата у - 
+Координата z -
+Функции - члены:
+Конструкторы:
+Конструктор по умолчанию
+Параметризированный конструктор с одним параметром -
+Параметризированный конструктор с тремя параметрами -
+Методы:
+Метод без входных и выходных параметров:
+Ввод вектора - 
+Вывод вектора -
+Метод только с выходными параметрами:
+Возвращает х -
+Возвращает у -
+Возвращает z -
+Возвращает длину вектора -
+Определение угла поворота вектора
+Метод только с входными параметрами:
+Увеличение координат вектора на скаляр -
+Уменьшение координат вектора на скаляр - 
+Метод с входными и выходными параметрами:
+Сложение двух векторов -
+Вычитание двух векторов -
+Поэлементное умножение двух векторов
+Скалярное произведение двух векторов
+Определение угла между двумя векторами
+Проверка на равенство двух векторов*/
+
+
+namespace Vector
+{    class MyVector
+    {
+        public double x { get; set; }
+        public double y { get; set; }
+        public double z { get; set; }
+
+        public MyVector(double x, double y, double z):this(y,z)
+        {
+           this.x = x;
+        }
+        public MyVector(double y, double z) : this(z)
+        {
+            this.y = y;
+        }
+        public MyVector(double z)
+        {
+            this.z = z;
+        }
+        public MyVector() 
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+        public double Init()
+        {
+            Console.WriteLine($"Введите координату х ->");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Введите координату y ->");
+            y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Введите координату z ->");
+            z = Convert.ToDouble(Console.ReadLine());
+            return 0;
+        }
+        public void Prdouble()
+        {
+            Console.WriteLine($"Координата х: {x}, y: {y}, z:{z}");
+        }
+        public double LenghtVector()
+        {
+            return Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2) + Math.Pow(z, 2));
+        }
+        public MyVector Increase(double a)
+        {
+            MyVector dop = new MyVector();
+            dop.x += a;
+            dop.y += a;
+            dop.z += a;
+            return dop;
+        }
+        public MyVector Decrease(double a)
+        {
+            MyVector dop = new MyVector();
+            dop.x += a;
+            dop.y += a;
+            dop.z += a;
+            return dop;
+        }
+        public MyVector Addition (MyVector b)
+        {
+            x += b.x;
+            y += b.y;
+            z += b.z;
+            return this;
+        }
+        public MyVector Subtraction(MyVector b)
+        {
+            x -= b.x;
+            y -= b.y;
+            z -= b.z;
+            return this;
+        }
+        public MyVector Multiplication()
+        {            
+            return this;
+        }
+    }
+}
